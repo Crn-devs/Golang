a3752fb1592ac1afa032a960a00419a5c2996cfb
Crn-devs <crn.develops@gmail.com>
2024-11-18 01:10:53 +0000

    functions and return types

--- Cards/main.go
+++ Cards/main.go
@@ -4,25 +4,18 @@ import "fmt"
 
 func main() {
 
-	// Variables
+	card := newCard()
 
-	var card string = "Ace of spades"
-
-	// short assigment type is infered from the initalised value
-	card2 := "Nine of Hearts"
-
-	// types can be explicit or inferred if the variable is initalised with a value
-
-	// un-initalised variables
-	//var card3 string
-	//card3 = "card3"
-
-	// bulk variable declaration
-	var (
-		card3 string = "three"
-		card4 string = "four"
-		card5 string = "five"
-	)
-
-	fmt.Println(card, card2, card3, card4, card5)
+	fmt.Println(card)
 }
+
+// Syntax
+// defines as a function || define the name || defines return type || function body
+func newCard() string {
+	return "this is a new card"
+}
+func newCardDeck() int {
+	return 52
+}
+
+// functions can be called from other files if they are in the same package
