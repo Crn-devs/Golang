1bd373205b92a68da34a5ce249ded5ba3e7c71e5
Crn-devs <crn.develops@gmail.com>
2024-11-18 00:52:52 +0000

    imports and variables in go

--- Cards/main.go
+++ Cards/main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+
+	// Variables
+
+	var card string = "Ace of spades"
+
+	// short assigment type is infered from the initalised value
+	card2 := "Nine of Hearts"
+
+	// types can be explicit or inferred if the variable is initalised with a value
+
+	// un-initalised variables
+	//var card3 string
+	//card3 = "card3"
+
+	// bulk variable declaration
+	var (
+		card3 string = "three"
+		card4 string = "four"
+		card5 string = "five"
+	)
+
+	fmt.Println(card, card2, card3, card4, card5)
 }
